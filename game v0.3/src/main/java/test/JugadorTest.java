95ae651cd703b210e3fa32541405f987dd2835d0
Ziro <ziro18@gmail.com>
2019-09-27 23:41:07 -0300

    merge jugador test

--- game v0.3/src/main/java/test/JugadorTest.java
+++ game v0.3/src/main/java/test/JugadorTest.java
@@ -1,9 +1,9 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-<<<<<<< HEAD
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import game.Casillero;
@@ -14,66 +14,6 @@ import game.Poder;
 import game.Tablero;
 
 public class JugadorTest {
-	private Jugador jugador  = new Jugador(1, new Tablero(1, 1), "asd");
-	
-	@Test
-	public void tirarDadoTest() {
-		DadoTrucado dadoTrucado = new DadoTrucado();
-		assertEquals(1, jugador.tirarDado(dadoTrucado));
-	}
-	
-	@Test
-	public void elegirCaminoTest() {
-		assertTrue(true);
-//		Casillero elegirCamino = jugador.elegirCamino(new Casillero(0, null, null, null, null, new Objeto(0), new Poder(0)));
-//		assertEquals(new Casillero(0, null, null, null, null, new Objeto(0), new Poder(0)), elegirCamino);
-	}
-	
-	@Test
-	public void usarObjetoTest() {
-		jugador.setObjeto(new Objeto(1));
-		assertEquals(new Objeto(1), jugador.usarObjeto());
-	}
-	
-	@Test
-	public void usarPoderTest() {
-		jugador.setPoder(new Poder(1));
-//		assertEquals(new Poder(1).efecto(jugador), jugador.usarPoder());
-		assertTrue(true);
-	}
-	
-	@Test
-	public void activarPoderTest() {
-		jugador.setPoder(new Poder(1));
-		assertEquals(new Poder(1), jugador.activarPoder());
-	}
-	
-	@Test
-	public void accionTest() {
-		assertTrue(jugador.accion());
-	}
-	
-	@Test
-	public void avanzarTest() {
-		jugador.setPosicionActual(new Casillero(0, null, null, null, null, new Objeto(0), new Poder(0)));
-		assertTrue(jugador.avanzar());
-	}
-	
-	@Test
-	public void activarCasillaTest() {
-		//no se que tiene que hacer!
-		assertTrue(true);
-=======
-
-import org.junit.Before;
-import org.junit.Test;
-
-import game.DadoTrucado;
-import game.Jugador;
-import game.Tablero;
-
-public class JugadorTest {
-
 	Tablero mapa = null;
 	Jugador jugador = null;
 	Jugador segundoJugador = null;
@@ -111,6 +51,56 @@ public class JugadorTest {
 		segundoJugador.setFinTurno(false);
 		segundoJugador.avanzar();
 		assertEquals(mapa.getCasillaFinal(), segundoJugador.getPosicionActual());
->>>>>>> 997a7914066601b76f72e6c27f4da0f8464b37c1
+	}
+	
+	@Test
+	public void tirarDadoTest() {
+		DadoTrucado dadoTrucado = new DadoTrucado();
+		assertEquals(1, jugador.tirarDado(dadoTrucado));
+	}
+	
+	@Test
+	public void elegirCaminoTest() {
+		assertTrue(true);
+//		Casillero elegirCamino = jugador.elegirCamino(new Casillero(0, null, null, null, null, new Objeto(0), new Poder(0)));
+//		assertEquals(new Casillero(0, null, null, null, null, new Objeto(0), new Poder(0)), elegirCamino);
+	}
+	
+	@Test
+	public void usarObjetoTest() {
+		Objeto objeto = new Objeto(1);
+		jugador.setObjeto(objeto);
+		assertEquals(objeto, jugador.usarObjeto());
+	}
+	
+	@Test
+	public void usarPoderTest() {
+		jugador.setPoder(new Poder(1));
+//		assertEquals(new Poder(1).efecto(jugador), jugador.usarPoder());
+		assertTrue(true);
+	}
+	
+	@Test
+	public void activarPoderTest() {
+		Poder poder = new Poder(1);
+		jugador.setPoder(poder);
+		assertEquals(poder, jugador.activarPoder());
+	}
+	
+	@Test
+	public void accionTest() {
+		assertTrue(jugador.accion());
+	}
+	
+	@Test
+	public void avanzarTest() {
+		jugador.setPosicionActual(new Casillero(0, null, null, null, null, new Objeto(0), new Poder(0)));
+		assertTrue(jugador.avanzar());
+	}
+	
+	@Test
+	public void activarCasillaTest() {
+		//no se que tiene que hacer!
+		assertTrue(true);
 	}
 }
