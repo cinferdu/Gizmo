ec99fa8a121a5e6d0e8c97495292bd42c39594ea
Ziro <ziro18@gmail.com>
2019-09-27 22:35:41 -0300

    test dado

--- game v0.3/src/main/java/game/Dado.java
+++ game v0.3/src/main/java/game/Dado.java
@@ -1,6 +1,7 @@
 package game;
 
 public class Dado {
+
 	private int caras;
 	
 	public Dado() {
@@ -10,5 +11,35 @@ public class Dado {
 
 	public int generar() {
 		return (int) Math.ceil((Math.random()*100) % this.caras);
+	}
+	
+	public int getCaras() {
+		return caras;
+	}
+	
+	public void setCaras(int caras) {
+		this.caras = caras;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + caras;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dado other = (Dado) obj;
+		if (caras != other.caras)
+			return false;
+		return true;
 	}
 }
--- game v0.3/src/main/java/game/DadoTrucado.java
+++ game v0.3/src/main/java/game/DadoTrucado.java
@@ -1,13 +1,8 @@
 package game;
 
 public class DadoTrucado extends Dado{
-private int caras;
 	
 	public DadoTrucado() {
-		this.caras = 1;
-	}
-
-	public int generar() {
-		return 1;
+		this.setCaras(1);
 	}
 }
