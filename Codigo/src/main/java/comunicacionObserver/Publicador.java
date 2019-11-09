0c3a4b646832c24859b85873af853b2c01ac1123
Ziro <ziro18@gmail.com>
2019-11-08 22:40:41 -0300

    fix

--- Codigo/src/main/java/comunicacionObserver/Publicador.java
+++
@@ -1,9 +0,0 @@
-package comunicacionObserver;
-
-import game.Jugador;
-
-public interface Publicador {
-	public void registrar(Suscriptor obs);
-	public void desregistrar(Suscriptor obs);
-	public void avisar(Operacion operacion,Jugador jugadorActual);
-}
